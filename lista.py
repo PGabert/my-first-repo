bfbc22e79de26d51bf1b014872c3f89aeb374a94
PGabert <151089919+PGabert@users.noreply.github.com>
2023-11-18 13:38:56 -0600

    lista.py

--- lista.py
+++ lista.py
@@ -1,5 +1,3 @@
-
-
 def suma_lista(lista):
     suma = 0
     for elemento in lista:
@@ -8,4 +6,15 @@ def suma_lista(lista):
 mi_lista = [1, 2, 3, 4, 5]
 resultado = suma_lista(mi_lista)
 print("El resultado total:", resultado)
+
+def multi_lista(lista):
+    multi = 1  # Cambié el valor inicial a 1 para que la multiplicación funcione correctamente
+    for elemento in lista:
+        multi *= elemento
+    return multi
+
+mi_lista = [2, 4, 5, 7, 9, 25, 8]
+resultado = multi_lista(mi_lista)
+print("El resultado de toda la lista:", resultado)
+
 
